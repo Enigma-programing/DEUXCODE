a33e2330618ea04987433540c06b6aa3556d8f61
Enigma-programing <bramdon.programing@gmail.com>
2024-10-29 07:48:29 -0500

    Aun esta en fase prueba y mejora
    No se le han realizado cambios

---
+++ REPOSITORIO PRINCIPAL/Contacto.js
@@ -0,0 +1,11 @@
+// Obtén el botón por su ID
+const boton = document.getElementById('miBoton');
+
+// Define la función que quieres ejecutar
+function miFuncion() {
+    alert('¡Botón clickeado!');
+}
+
+// Añade un evento 'click' al botón
+boton.addEventListener('index_Contacto.js', miFuncion);
+
